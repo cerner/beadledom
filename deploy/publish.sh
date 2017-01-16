561f7824b58df803b1ac24e1c73f493d42fa74d0
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-16 09:54:17 -0600

    Update the release documentation

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
+#!/bin/env bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
+if [ -z "$SONATYPE_USERNAME" ]
+then
+    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+    exit 1
 fi
+
+if [ -z "$SONATYPE_PASSWORD" ]
+then
+    echo "error: please set SONATYPE_PASSWORD environment variable"
+    exit 1
+fi
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings $GPG_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+fi
+
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
