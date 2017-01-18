12e5a335f22618c67f2c1bc208b1f558ed3d5bd4
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-18 13:34:12 -0600

    commented the build on PR

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -12,10 +12,10 @@ then
     exit 1
 fi
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
-fi
+#if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+#    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+#    exit $?
+#fi
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
