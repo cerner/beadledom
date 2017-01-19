39e3da51a22d6c0e35046ba24f81437bccaa8894
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-19 09:18:23 -0600

    debugging the travis script

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 if [ -z "$SONATYPE_USERNAME" ]
 then
     echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
@@ -16,8 +17,12 @@ if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings $GPG_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn clean deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    exit $?
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
+    mvn clean deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+    exit $?
 fi
 
-mvn clean deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+
