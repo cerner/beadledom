18d9317b875ac49eac9202a6a65b3d677ec289ea
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-19 13:56:11 -0600

    fixed the path

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -10,10 +10,10 @@ then
     echo "Checking out a tag $TRAVIS_TAG"
     git checkout tags/$TRAVIS_TAG -b tag_$TRAVIS_TAG
     mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-    ./publish_docs.sh $TRAVIS_TAG
+    ${GPG_DIR}/publish_docs.sh $TRAVIS_TAG
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
     #mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -Dgpg.skip -B -U
-    sh ./publish_docs.sh dev
+    ${GPG_DIR}/publish_docs.sh dev
 fi
 
