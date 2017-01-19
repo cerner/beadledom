862ab691f94e02b1de1df877ee94f9d01f118d39
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-19 10:01:00 -0600

    more changes to build

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
-
-if [ -z "$SONATYPE_USERNAME" ]
-then
-    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
-
-if [ -z "$SONATYPE_PASSWORD" ]
-then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
+echo "executing deploy.sh"
+echo "decrypting keyrings"
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings $GPG_DIR/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
-    mvn clean deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    exit $?
+
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
-    mvn clean deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-    exit $?
 fi
 
-
+mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
