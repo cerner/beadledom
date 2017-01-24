dc4a7bdb809c0e29d7aae77803f25d88b730e297
Sundeep Paruvu <sparuvu@gmail.com>
2017-01-24 09:40:08 -0600

    Update Travis configuration + release documentation (#3)
    * Initial Commit
    
    * Update the release documentation
    
    * updated the release doc
    
    * removed the duplicate dependencies
    
    * updated the travis
    
    * re encrypted the gpg secret + key
    
    * ls the contents of gpg_dir for debugging
    
    * updated the publish.sh + replced after_success with before_install
    
    * commented the build on PR
    
    * updated the releasing doc
    
    * fixed the bad merge
    
    * change the build branch
    
    * update the travis + profile settings
    
    * pre review clean up
    
    * reverting all the non-sense
    
    * debugging the travis script
    
    * fixed a typo
    
    * more changes to build
    
    * use -X
    
    * more updates to the publish script
    
    * automatically publish docs
    
    * comment the build for snapshot
    
    * fixed the path
    
    * dont install the project yet
    
    * run maven in denug mode
    
    * fetch before creating a branch
    
    * fetch before creating a branch
    
    * split the branch checkout
    
    * skip ITs
    
    * updated the variable name
    
    * rmmove skitIt flag
    
    * manually set the remote
    
    * set the gh-pages branch fetch url
    
    * call the bash script directly
    
    * call the index.sh with bash
    
    * update the global git config
    
    * revert the builds to snapshot to master
    
    * one final update to docs
    
    * removed the extra line at the end of the file
    
    * updates as per review comments
    
    * removed the unnecessary environemtn variable
    
    * updated release documentation - removed signing up with cerner email part.
    
    * build only master
    
    * moved decrypting secrets to before_deploy block
    
    * don't shut off the output of mvn site command
    
    * removed the extra slash

---
+++ .travis/publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "deploying beadledom $TRAVIS_TAG to maven central"
+    git checkout tags/$TRAVIS_TAG -b tag_$TRAVIS_TAG
+    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+    ${GPG_DIR}/publish_docs.sh $TRAVIS_TAG
+else
+    echo "deploying SNAPSHOT from master"
+    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -Dgpg.skip -B -U
+fi
