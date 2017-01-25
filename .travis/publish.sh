a422cad12982d5030b5c05079933a9571df83aee
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-25 13:00:24 -0600

    updated travis configuratoin

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "deploying beadledom $TRAVIS_TAG to maven central"
-    git checkout tags/$TRAVIS_TAG -b tag_$TRAVIS_TAG
-    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-    ${GPG_DIR}/publish_docs.sh $TRAVIS_TAG
-else
-    echo "deploying SNAPSHOT from master"
-    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -Dgpg.skip -B -U
-fi
+echo "deploying beadledom $TRAVIS_TAG to maven central"
+mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+${GPG_DIR}/publish_docs.sh 2.3
