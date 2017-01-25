982252da4d6d7df6107c00cbdeddbc9b537489e4
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-25 13:07:22 -0600

    added the -x to mvn command

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "deploying beadledom $TRAVIS_TAG to maven central"
-mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U -X
 ${GPG_DIR}/publish_docs.sh 2.3
