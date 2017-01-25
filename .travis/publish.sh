5043595532ff33144115f990b9e37c3a5ede6e21
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-25 13:46:02 -0600

    turn offmaven debug option

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "deploying beadledom $TRAVIS_TAG to maven central"
-mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U -X
+mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
 ${GPG_DIR}/publish_docs.sh 2.3
