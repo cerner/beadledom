a2905a92847989000180fc4a1db653862e6d164d
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-25 17:20:16 -0600

    more updates

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo "deploying beadledom $TRAVIS_TAG to maven central"
+echo "deploying beadledom 2.3-Beta to maven central"
 mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-${GPG_DIR}/publish_docs.sh 2.3
+${GPG_DIR}/publish_docs.sh 2.3-Beta
