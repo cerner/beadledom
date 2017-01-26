61bd792900c140fd63d1e653cebd16429fcec436
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-26 13:36:55 -0600

    activate by prooperty name

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 echo "deploying beadledom 2.3-Beta to maven central"
-#mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-echo "printing active-profiles"
-mvn help:active-profiles --settings $GPG_DIR/settings.xml
+mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -DattachScaladocs=true -B -U
 #${GPG_DIR}/publish_docs.sh 2.3-Beta
