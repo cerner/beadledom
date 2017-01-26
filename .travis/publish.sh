b41923286cbf7d8b8b2a9344b7d3e6e609c65409
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-26 13:17:53 -0600

    print the actrive profiles

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 echo "deploying beadledom 2.3-Beta to maven central"
-mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-${GPG_DIR}/publish_docs.sh 2.3-Beta
+#mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
+echo "printing active-profiles"
+mvn help:active-profiles --settings $GPG_DIR/settings.xml
+#${GPG_DIR}/publish_docs.sh 2.3-Beta
