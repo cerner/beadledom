b0bfdcbc3b4464efdad75eb9e93435e475d93525
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-27 11:13:51 -0600

    renamed publish_docs to publish_site

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -5,7 +5,7 @@ then
     echo "deploying $TRAVIS_TAG to maven central"
     git checkout tags/$TRAVIS_TAG -b tag_$TRAVIS_TAG
     mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -DattachScaladocs=true -B -U
-    ${GPG_DIR}/publish_docs.sh $TRAVIS_TAG
+    ${GPG_DIR}/publish_site.sh $TRAVIS_TAG
 else
     echo "deploying SNAPSHOT version from master"
     mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -Dgpg.skip -B -U
