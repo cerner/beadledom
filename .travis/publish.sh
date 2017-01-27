acad6d9dfebcf347334a6e8a3c5a06efb6f306a5
Sundeep Paruvu <sparuvu@gmail.com>
2017-01-27 16:36:50 -0600

    Fix the Sonatype validation issues. (#11)
    Fix the Sonatype validation issues - (javadoc, scaladoc, signing the artifacts etc)

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ ! -z "$TRAVIS_TAG" ]
 then
-    echo "deploying beadledom $TRAVIS_TAG to maven central"
-    git checkout tags/$TRAVIS_TAG -b tag_$TRAVIS_TAG
-    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -B -U
-    ${GPG_DIR}/publish_docs.sh $TRAVIS_TAG
+    echo "Decrypting secrets ..."
+    openssl aes-256-cbc -K $encrypted_4d3aca009c62_key -iv $encrypted_4d3aca009c62_iv -in $GPG_DIR/deploy_site_key.enc -out $GPG_DIR/deploy_site_key -d
+    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
+    openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d;
+
+    echo "deploying $TRAVIS_TAG to maven central"
+    mvn deploy --settings $GPG_DIR/settings.xml -DattachScaladocs=true -B -U
+
+    echo "building site"
+    ${GPG_DIR}/publish_site.sh $TRAVIS_TAG
 else
     echo "deploying SNAPSHOT from master"
-    mvn deploy --settings $GPG_DIR/settings.xml -DskipTests=true -Dgpg.skip -B -U
+    mvn deploy --settings $GPG_DIR/settings.xml -Dgpg.skip -B -U
 fi
