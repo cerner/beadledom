a2f879b5f3f14a8bc1edec98f118df309a2ca2e6
Paruvu,Sundeep <sparuvu@gmail.com>
2017-01-26 21:32:59 -0600

    optimized travis

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "$(echo 'VERSION=${project.version}' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION')" == *-SNAPSHOT ]]; then $(export IS_SNAPSHOT=true; fi
+if [[ "$(echo 'VERSION=${project.version}' | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate | grep '^VERSION')" == *-SNAPSHOT ]];
+  then
+    IS_SNAPSHOT=true;
+fi
+if [ ! ${IS_SNAPSHOT} ];
+then
+  echo "stuff"
+else
+  echo "ding"
+fi
