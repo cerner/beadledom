9de5da23fa61a28506d01d82ff72736a8b6f9dc4
John Leacox <john.leacox@cerner.com>
2018-01-11 16:49:04 -0600

    Update archetype and documentation for new recommended client structure.
    The new recommend client structure is to have the API interface defined
    in the client for automatic proxying via beadledom-client. This differs
    from the previous guidance which was to have this interface in the api
    project to be shared between the client and service. Sharing the
    interface causes issues due to the fact that the service side
    implementation can have only one method per path/method combo, making
    it very hard to passively add support for new query parameters. By
    separating the client and service implementations, the service can
    add query parameters as needed and the client can add new methods
    to the proxied interface for different variations of query parameters.

--- archetype/simple-service/src/main/resources/archetype-resources/client/src/main/java/client/HelloWorldResource.java
+++ archetype/simple-service/src/main/resources/archetype-resources/client/src/main/java/client/HelloWorldResource.java
@@ -1,26 +1,18 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.api;
+package ${package}.client;
 
 import ${package}.api.model.HelloWorldDto;
 import com.cerner.beadledom.jaxrs.GenericResponse;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Api(value = "/hello", description = "Retrieve hello world data")
 @Path("/hello")
 public interface HelloWorldResource {
 
-  @ApiOperation(
-      value = "Retrieves hello world data.",
-      response = HelloWorldDto.class)
-  @ApiResponse(code = 200, message = "Success")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   GenericResponse<HelloWorldDto> getHelloWorld();
--- archetype/simple-service/src/main/resources/archetype-resources/service/src/main/java/service/__name__Module.java
+++ archetype/simple-service/src/main/resources/archetype-resources/service/src/main/java/service/__name__Module.java
@@ -3,14 +3,17 @@
 #set( $symbol_escape = '\' )
 package ${package}.service;
 
-import ${package}.service.resource.HelloWorldResourceImpl;
-import ${package}.api.HelloWorldResource;
+import ${package}.service.resource.HelloWorldResource;
 import com.google.inject.PrivateModule;
 
 public class ${name}Module extends PrivateModule {
 
   protected void configure() {
-    bind(HelloWorldResource.class).to(HelloWorldResourceImpl.class);
+    bind(HelloWorldResource.class);
+
+    // Note: All clients that will be used should be installed in this module to avoid registering
+    // client JAX-RS resources with the server. The Correlation ID header name for clients could
+    // also be configured/overridden here.
 
     expose(HelloWorldResource.class);
   }
