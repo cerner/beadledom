10c5c542397e9aac196a0ddda21cfaccd147670f
John Leacox <johnlcox@users.noreply.github.com>
2018-04-11 16:45:24 -0500

    Update with longer lived GPG signing key (#90)

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy --settings $CI_DIR/settings.xml -DperformRelease=true -DskipTests=true
     exit $?
 fi
