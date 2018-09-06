a09c2909710f8e64c49179b318e4223ff9fb9dee
Kottman,Ian <Ian.Kottman@Cerner.com>
2018-09-06 10:29:00 -0500

    Remove unncessary annotations

--- client/beadledom-client-example/example-api/src/main/java/com/cerner/beadledom/client/example/PaginatedClientResource.java
+++ client/beadledom-client-example/example-api/src/main/java/com/cerner/beadledom/client/example/PaginatedClientResource.java
@@ -4,7 +4,6 @@ import com.cerner.beadledom.client.example.model.JsonOne;
 import com.cerner.beadledom.jaxrs.GenericResponse;
 import com.cerner.beadledom.pagination.models.OffsetPaginatedListDto;
 
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -20,6 +19,6 @@ public interface PaginatedClientResource {
   @GET
   @Produces("application/json")
   GenericResponse<OffsetPaginatedListDto<JsonOne>> index(
-      @DefaultValue("0") @QueryParam("offset") final Long offset,
-      @DefaultValue("20") @QueryParam("limit") final Integer limit);
+      @QueryParam("offset") final Long offset,
+      @QueryParam("limit") final Integer limit);
 }
