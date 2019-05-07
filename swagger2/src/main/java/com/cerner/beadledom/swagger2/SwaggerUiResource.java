9604250cb0d238b3b3a54e545e5f93b71cc40f1d
nb051436 <nick.behrens@cerner.com>
2019-05-07 15:37:28 -0500

    Add back security context and http headers with print statements to figure out whats going wrong behind reverse proxy

--- swagger2/src/main/java/com/cerner/beadledom/swagger2/SwaggerUiResource.java
+++ swagger2/src/main/java/com/cerner/beadledom/swagger2/SwaggerUiResource.java
@@ -4,11 +4,14 @@ import com.cerner.beadledom.jaxrs.StreamingWriterOutput;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.MustacheFactory;
 import com.google.common.collect.ImmutableMap;
+import java.util.regex.Pattern;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
 
@@ -20,12 +23,35 @@ public class SwaggerUiResource {
   @GET
   @Path("/ui")
   @Produces(MediaType.TEXT_HTML)
-  public StreamingOutput getSwaggerUi(@Context UriInfo uriInfo) {
+  public StreamingOutput getSwaggerUi(@Context UriInfo uriInfo,
+      @Context SecurityContext securityContext, @Context HttpHeaders httpHeaders) {
+    System.out.println("Base URI: " + uriInfo.getBaseUri().toString());
+    System.out.println("Request URI: " + uriInfo.getRequestUri().toString());
+    System.out.println("Request Headers: ");
+    httpHeaders.getRequestHeaders().forEach((key, value) -> System.out.println("Header Key: "+ key + ", Header value: "+ value));
+    String httpScheme = isRequestSecure(securityContext, httpHeaders) ? "https" : "http";
     return StreamingWriterOutput.with(writer -> MUSTACHE_FACTORY.compile("ui.mustache").execute(
         writer, ImmutableMap.of(
             "apiDocsUrl",
-            uriInfo.getBaseUriBuilder().scheme(uriInfo.getBaseUri().getScheme()).path(SwaggerApiResource.class).build().toASCIIString()))
+            uriInfo.getBaseUriBuilder().scheme(httpScheme).path(SwaggerApiResource.class).build().toASCIIString()))
     );
+  }
+
+  private boolean isRequestSecure(SecurityContext securityContext, HttpHeaders httpHeaders) {
+    return securityContext.isSecure()
+        || secureForwardedHeader(httpHeaders.getRequestHeaders().getFirst("Forwarded"))
+        || secureXForwardedProtoHeader(httpHeaders.getRequestHeaders().getFirst("X-Forwarded-Proto"));
+  }
+
+  private boolean secureForwardedHeader(String forwardedHeader) {
+    Pattern forwardedPairs = Pattern.compile("proto=(?<protocolValue>[^;]*)(;|\\z)");
+
+    return forwardedHeader != null
+        && forwardedPairs.matcher(forwardedHeader).group("protocolValue").contains("https");
+  }
+
+  private boolean secureXForwardedProtoHeader(String xForwardedProtoHeader) {
+    return xForwardedProtoHeader != null && xForwardedProtoHeader.equals("https");
   }
 }
 
