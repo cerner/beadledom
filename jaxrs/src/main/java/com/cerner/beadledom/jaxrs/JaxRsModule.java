d59412454912e9eaa344f9ef24052827ed47bd94
nb051436 <nick.behrens@cerner.com>
2019-08-01 16:04:42 -0500

    Removing the install by default, adding documentation in the jaxrs manual for how to use it

--- jaxrs/src/main/java/com/cerner/beadledom/jaxrs/JaxRsModule.java
+++ jaxrs/src/main/java/com/cerner/beadledom/jaxrs/JaxRsModule.java
@@ -8,12 +8,6 @@ import javax.inject.Singleton;
 /**
  * A guice module for providing jax-rs specific bindings.
  *
- * <p>Binds:
- * <ul>
- *   <li>{@link ForwardedHeaderFilter}</li>
- * </ul>
- * </p>
- *
  * <p>Provides:
  * <ul>
  *     <li>{@link CorrelationIdFilter}</li>
@@ -26,6 +20,5 @@ public class JaxRsModule extends AbstractModule {
   protected void configure() {
     bind(CorrelationIdFilter.class).toProvider(CorrelationIdFilterProvider.class)
         .in(Singleton.class);
-    bind(ForwardedHeaderFilter.class).in(Singleton.class);
   }
 }
