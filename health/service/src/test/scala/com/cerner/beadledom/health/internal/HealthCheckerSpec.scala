43c5106693d26f8f447b3087d526b52364da394d
John Leacox <john.leacox@cerner.com>
2020-03-27 11:58:17 -0500

    Upgrade to Scala 2.13

--- health/service/src/test/scala/com/cerner/beadledom/health/internal/HealthCheckerSpec.scala
+++ health/service/src/test/scala/com/cerner/beadledom/health/internal/HealthCheckerSpec.scala
@@ -12,7 +12,7 @@ import javax.ws.rs.WebApplicationException
 import org.jboss.resteasy.spi.ResteasyUriInfo
 import org.mockito.Mockito.when
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.must.Matchers
@@ -34,7 +34,7 @@ class HealthCheckerSpec extends AnyFunSpec with Matchers with MockitoSugar {
   def newChecker(requestUri: String, dependencies: List[HealthDependency]) = new HealthChecker(
     new ResteasyUriInfo(new URI(requestUri)),
     metadata,
-    dependencies.groupBy(d => d.getName).mapValues(ds => ds.head).asJava
+    dependencies.groupBy(d => d.getName).view.mapValues(ds => ds.head).toMap.asJava
   )
 
   def newDependency(name: String, desc: String,
