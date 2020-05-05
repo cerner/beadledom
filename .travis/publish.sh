e70ccde799e27542ab3fe1e3b36c310f4cc48d03
Venkatesh Sridharan <venkatesh.sridharan@cerner.com>
2020-05-04 20:07:39 -0500

    Don't decrypt site key

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "Decrypting secrets ..."
-    openssl aes-256-cbc -K $encrypted_4d3aca009c62_key -iv $encrypted_4d3aca009c62_iv -in $CI_DIR/deploy_site_key.enc -out $CI_DIR/deploy_site_key -d
+#    openssl aes-256-cbc -K $encrypted_4d3aca009c62_key -iv $encrypted_4d3aca009c62_iv -in $CI_DIR/deploy_site_key.enc -out $CI_DIR/deploy_site_key -d
     openssl aes-256-cbc -K $encrypted_0753278e989f_key -iv $encrypted_0753278e989f_iv -in $CI_DIR/signingkey.asc.enc -out $CI_DIR/signingkey.asc -d
 
     echo "Importing GPG signing key"
